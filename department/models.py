3959484b0cbf90096fa7f125643b6da3e4e27222
reshma-golandaj <reshmasgolandaj@gmail.com>
2026-07-06 17:21:45 +0530

    Add department model and admin configuration

---
+++ department/models.py
@@ -0,0 +1,11 @@
+import uuid
+from django.db import models
+
+class Department(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    company=models.ForeignKey("tenants.Company",on_delete=models.CASCADE,related_name="department")
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True,null=True)
+    is_active=models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
