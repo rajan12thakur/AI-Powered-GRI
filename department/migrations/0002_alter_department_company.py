b7bd0b54f05403804027ae7a7982d412727e4cf0
reshma-golandaj <reshmasgolandaj@gmail.com>
2026-07-06 18:26:58 +0530

    changes after creating company application

--- department/migrations/0002_alter_department_company.py
+++ department/migrations/0002_alter_department_company.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='department',
             name='company',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='department', to='tenants.company'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='department', to='company.company'),
         ),
     ]
--- facility/admin.py
+++ facility/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Company
-admin.site.register(Company)
+#from .models import Facility
+#admin.site.register(Facility)
 # Register your models here.
